c9684986bf050329a965c00fdd41d76add6d2fe4
Geoffrey.C <cycjimmy@gmail.com>
2021-11-22 21:05:18 +0800

    feat: add function designPx2Px

--- release.config.js
+++ release.config.js
@@ -1,3 +1,3 @@
-const makeCommonConfig = require('@cycjimmy/config-lib/semanticRelease/15.x/makeCommonConfig');
+const makeCommonConfig = require('@cycjimmy/config-lib/cjs/semanticRelease/15.x/makeCommonConfig').default;
 
 module.exports = makeCommonConfig();
