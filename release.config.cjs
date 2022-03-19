6546a7be83efbd224b934cb16dc3cbab9f99bc6d
Geoffrey.C <cycjimmy@gmail.com>
2022-03-19 15:46:07 +0800

    feat: change to module mode
    change to module mode
    
    BREAKING CHANGE: change to module mode

---
+++ release.config.cjs
@@ -0,0 +1,3 @@
+const makeCommonConfig = require('@cycjimmy/config-lib/cjs/semanticRelease/15.x/makeCommonConfig.cjs').default;
+
+module.exports = makeCommonConfig();
--- release.config.js
+++
@@ -1,3 +0,0 @@
-const makeCommonConfig = require('@cycjimmy/config-lib/cjs/semanticRelease/15.x/makeCommonConfig').default;
-
-module.exports = makeCommonConfig();
