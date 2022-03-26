aca3f68c64d1ebfaae350175dcb003b3ff1f53f4
cycjimmy <cycjimmy@gmail.com>
2022-03-26 21:51:25 +0800

    chore: some chore

--- release.config.cjs
+++ release.config.cjs
@@ -1,4 +1,4 @@
 /* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
-const makeCommon = require('@cycjimmy/config-lib/cjs/semanticRelease/19.x/makeConfigWithPgkRootForLibrary.cjs').default;
+const makeConfig = require('@cycjimmy/config-lib/cjs/semanticRelease/19.x/makeConfigWithPgkRootForLibrary.cjs').default;
 
-module.exports = makeCommon();
+module.exports = makeConfig();
